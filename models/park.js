14c280b18df84c5d6c1c428c83fddf6b73b4df65
Nuno Farinha <nadfarinha@gmail.com>
2019-02-19 20:45:16 +0000

    Create function to remove a dinosaur from park

--- models/park.js
+++ models/park.js
@@ -4,4 +4,16 @@ const Park = function(name, ticketPrice, dinosaurs) {
   this.dinosaurs = dinosaurs;
 };
 
+Park.prototype.addDinosaur = function(newDinosaur) {
+  this.dinosaurs.push(newDinosaur);
+};
+
+//Based on https://stackoverflow.com/a/5767357/5621295
+Park.prototype.removeDinosaur = function(rmDinosaur) {
+  let index = this.dinosaurs.indexOf(rmDinosaur);
+  if (index > -1) {
+    this.dinosaurs.splice(index, 1);
+  };
+}
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -7,14 +7,17 @@ describe('Park', function() {
   let dinosaur1;
   let dinosaur2;
   let dinosaur3;
+  let dinosaur4;
   let dinos = [];
   let park;
 
   beforeEach(function () {
 
     dinosaur1 = new Dinosaur('t-rex', 'carnivore', 50);
-    dinosaur2 = new Dinosaur('triceratops', '', 45);
-    dinosaur3 = new Dinosaur('galimimus', '', 20);
+    dinosaur2 = new Dinosaur('triceratops', 'herbivore', 45);
+    dinosaur3 = new Dinosaur('gallimimus', 'herbivore', 20);
+
+    dinosaur4 = new Dinosaur('brachiosaurus', 'herbivore', 35);
 
     dinos = [dinosaur1, dinosaur2, dinosaur3];
 
@@ -31,11 +34,22 @@ describe('Park', function() {
     assert.strictEqual(actual, 500);
   });
 
-  it('should have a collection of dinosaurs');
+  it('should have a collection of dinosaurs', function() {
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual, 3);
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to add a dinosaur to its collection', function() {
+    park.addDinosaur(dinosaur4);
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual, 4);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function() {
+    park.removeDinosaur(dinosaur3);
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual, 2);
+  });
 
   it('should be able to find the dinosaur that attracts the most visitors');
 
